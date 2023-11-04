097e7d1c3aa9d6e08cb9668cbe53baef6a3c64a7
Bilal Mbarki <bilal.ftb2000@gmail.com>
2023-11-04 02:05:33 +0100

    fix port issue

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const app = require('express')();
 const date = new Date();
 const time = date.getHours() + " : " + date.getMinutes();
 
-app.listen()
+app.listen(
+    80,
+    () => console.log("API is Runnig")
+)
 
 app.get('/checkTime', (req, res) => {
     res.status(200).send({
