485abae74e9be58b1796fd93d943d2207c176ebe
Bilal Mbarki <bilal.ftb2000@gmail.com>
2023-11-04 21:32:50 +0100

    https again

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require('express');
-//const https = require('https');
-//const fs = require('fs');
+const https = require('https');
+const fs = require('fs');
 const date = new Date();
 const time = date.getHours() + " : " + date.getMinutes();
 const PORT = 3000;
 
-/*const options = {
+const options = {
     key: fs.readFileSync('./certs/key.key'),
     cert: fs.readFileSync('./certs/cert.pem')
-};*/
+};
 
 const app = express();
-//const server = https.createServer(options, app);
+const server = https.createServer(options, app);
 
 
 app.listen(
