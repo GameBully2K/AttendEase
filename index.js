b581127776c44145e72affd38536f96c08e07373
Bilal Mbarki <bilal.ftb2000@gmail.com>
2023-11-04 02:22:05 +0100

    edited ignore file

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const app = require('express')();
+const https = require('https')();
 const date = new Date();
 const time = date.getHours() + " : " + date.getMinutes();
 
+
+
 app.listen(
-    443,
+    80,
     () => console.log("API is Runnig")
 )
 
