3ffa8387b9c846cc5386c1fa7ac2a27c66eb0a1c
Bilal Mbarki <bilal.ftb2000@gmail.com>
2023-11-04 19:00:15 +0100

    fixed ssl certs path

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const app = require('express')();
-const https = require('https')();
-const fs = require('fs')();
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
 const date = new Date();
 const time = date.getHours() + " : " + date.getMinutes();
 const PORT = 443;
 
 const options = {
-    key: fs.readFileSync(''),
-    cert: fs.readFileSync('')
-}
+    key: fs.readFileSync('certs/key.key'),
+    cert: fs.readFileSync('certs/cert.pem')
+};
 
+const app = express();
 const server = https.createServer(options, app);
 
 
