79a104b6a37aa7043170bd3e7abe1defbde22032
Bilal Mbarki <bilal.ftb2000@gmail.com>
2024-01-22 01:49:05 +0100

    proved essential to run axios

--- index.js
+++ index.js
@@ -53,7 +53,14 @@ const options = {
 
 const app = express();
 app.use(express.json());
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 const server = https.createServer(options, app);
 
